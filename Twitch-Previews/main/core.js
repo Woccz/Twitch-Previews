42fe0c2ef2c1d778b8e4bd5fe93c6c41586e2e2e
Woccz <44993506+Woccz@users.noreply.github.com>
2022-04-06 22:12:36 +1200

    Invert balance adjustment

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -2438,7 +2438,8 @@
                                                     let l_balance = (lTrV/(lTrV + rTlV));
                                                     
                                                     // Non linear balance
-                                                    l_balance = l_balance < 0.5 ? (0.5 * l_balance/(1-l_balance)) : (1 - 0.5 * (1-l_balance)/l_balance);
+                                                    //l_balance = l_balance < 0.5 ? (0.5 * l_balance/(1-l_balance)) : (1 - 0.5 * (1-l_balance)/l_balance);
+                                                    l_balance = l_balance < 0.5 ?  2*l_balance/(2*l_balance+1): -1/(2*l_balance-3);
                                                     
                                                     let r_balance = 1-l_balance
                                                     
